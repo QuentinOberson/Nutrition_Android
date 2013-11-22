10b1b35fb32ad93f5dedb49522921e1b5dc90fbd
kevinwyer <kevinwyer@hotmail.ch>
2013-11-22 12:46:13 +0100

    Android registration

--- app/src/com/example/nutrition_guardian/MainActivity.java
+++ app/src/com/example/nutrition_guardian/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.nutrition_guardian;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	private Button buttonregister;
+	private Intent intent;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Aktion aktion = new Aktion();
+		
+		buttonregister = (Button)this.findViewById(R.id.button2);
+		intent = new Intent(this, RegisterActivity.class);
+		
+		buttonregister.setOnClickListener(aktion);
 	}
 
 	@Override
@@ -19,4 +33,14 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	private class Aktion implements OnClickListener{
+
+		@Override
+		public void onClick(View view) {
+			
+			if(view == buttonregister){
+				MainActivity.this.startActivity(intent);
+			}
+		}		
+	}	
 }
