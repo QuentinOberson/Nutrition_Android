577cd036d6db964eb68142bb4d76c1f7fd787068
vincentpont <vincentpont@gmail.com>
2013-12-02 13:46:03 +0100

    android

--- app/src/com/user/UserProfil.java
+++ app/src/com/user/UserProfil.java
@@ -1,17 +1,31 @@
 package com.user;
 
 import com.example.nutrition_guardian.R;
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.EditText;
 
 public class UserProfil extends Activity {
 
+	private EditText firstname ;
+	private EditText lastname;
+	private EditText email;
+	private EditText password ;
+	
+	
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_user_profil);
+		
+		firstname = (EditText) findViewById(R.id.editText1);
+		firstname = (EditText) findViewById(R.id.editText2);
+		firstname = (EditText) findViewById(R.id.editText3);
+		firstname = (EditText) findViewById(R.id.editText4);
+		
 	}
 
 	@Override
