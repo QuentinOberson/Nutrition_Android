ac7cdf618d9a86cb096c75c5c16c9dedae335413
kevinwyer <kevinwyer@hotmail.ch>
2013-12-06 13:21:30 +0100

    Android

--- app/src/com/nutrition_guardian/StatisticsActivity.java
+++ app/src/com/nutrition_guardian/StatisticsActivity.java
@@ -1,10 +1,15 @@
 package com.nutrition_guardian;
 
-import com.example.nutrition_guardian.R;
+import java.util.ArrayList;
+import java.util.List;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.nutrition_guardian.R;
 
 public class StatisticsActivity extends Activity {
 
@@ -12,13 +17,22 @@ public class StatisticsActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_statistics);
-	}
+		
+		List <String> ListStatistics = new ArrayList <String>();
+		
+		ListStatistics.add("hallo");
+		ListStatistics.add("ciao");
+		
+		ArrayAdapter<String> adapterStat = new ArrayAdapter<String>(StatisticsActivity.this, android.R.layout.simple_list_item_1, ListStatistics);
+		
+		ListView lStat = (ListView) findViewById(R.id.listView1);
+		lStat.setAdapter(adapterStat);
+	}	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.statistics, menu);
 		return true;
-	}
-
+	}	
 }
