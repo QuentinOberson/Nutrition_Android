7a2b8bf55928eda57fe8a9bb662c3b4a0266c84f
kevinwyer <kevinwyer@hotmail.ch>
2013-12-12 18:10:02 +0100

    android

--- app/src/com/nutrition_guardian/EnterPlatePartsActivity.java
+++ app/src/com/nutrition_guardian/EnterPlatePartsActivity.java
@@ -1,14 +1,21 @@
 package com.nutrition_guardian;
 
-import com.example.nutrition_guardian.R;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+
+import com.example.nutrition_guardian.R;
 
 public class EnterPlatePartsActivity extends Activity{
-	
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_enter_plate_parts);
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.enter_plates, menu);
+		return true;
 	}
 }
