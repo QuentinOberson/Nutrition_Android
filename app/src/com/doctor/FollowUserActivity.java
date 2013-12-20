b3b2cf2d80486c37dcd3ef48fd69efd7d2aa99e3
kevinwyer <kevinwyer@hotmail.ch>
2013-12-20 13:25:46 +0100

    android

--- app/src/com/doctor/FollowUserActivity.java
+++ app/src/com/doctor/FollowUserActivity.java
@@ -3,27 +3,43 @@ package com.doctor;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 import com.example.nutrition_guardian.R;
-import com.nutrition_guardian.EnterPlatePartsActivity;
 import com.nutrition_guardian.StatisticsActivity;
 
 public class FollowUserActivity extends Activity {
+
+	private Button buttonfollow;
+
+	private Intent intent_stat;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_follow_user);
-		
+
 		setTitle(R.string.titleFollowUser);
 
+		buttonfollow = (Button) this.findViewById(R.id.button1);
+
+		intent_stat = new Intent(this, StatisticsActivity.class);
+
+		// Statistic listener
+		OnClickListener button1 = new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				FollowUserActivity.this.startActivity(intent_stat);
+			}
+		};
+
+		buttonfollow.setOnClickListener(button1);
+
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
